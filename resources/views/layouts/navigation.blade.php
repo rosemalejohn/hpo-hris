985671b28cf258c6d164de26fff44dc804a1869d
Rosemale-John II C. Villacorta <rosemalejohn@gmail.com>
2015-08-10 16:46:39 +0800

    Layouts

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -33,6 +33,8 @@
             <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
         </a>
         <ul class="dropdown-menu dropdown-user">
+			<li><a href="#"><i class="fa fa-user fa-fw"></i> Logged in as {{ auth()->user()->name }}</a>
+            </li>
             <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
             </li>
             <li class="divider"></li>
