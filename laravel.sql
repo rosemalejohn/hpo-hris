86c5d7ad0e7d5ab323bc68d0bebc99a2b48d09d3
Rosemale-John II C. Villacorta <rosemalejohn@gmail.com>
2015-08-11 16:34:34 +0800

    Upload SQL

--- laravel.sql
+++ laravel.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 09, 2015 at 02:12 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: Aug 11, 2015 at 10:34 AM
+-- Server version: 5.6.20
+-- PHP Version: 5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `departments` (
   `description` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `departments`
@@ -44,7 +44,8 @@ INSERT INTO `departments` (`id`, `department_code`, `name`, `description`, `crea
 (2, 'ADMIN', 'Administrator', 'Admin office staffs', '2015-08-08 07:19:39', '0000-00-00 00:00:00'),
 (3, 'SysAd', 'System Administrator', 'Maintains technical problems', '2015-08-08 00:07:38', '2015-08-08 00:07:38'),
 (4, 'Sales&Mar', 'Sales and Marketing', '', '2015-08-08 06:15:58', '2015-08-08 06:15:58'),
-(5, 'QA', 'Quality Assurance', '', '2015-08-08 06:17:12', '2015-08-08 06:17:12');
+(5, 'QA', 'Quality Assurance', '', '2015-08-08 06:17:12', '2015-08-08 06:17:12'),
+(6, 'ENGAGEDIQ', 'Engaged IQ', 'Sample ', '2015-08-10 00:34:27', '2015-08-10 00:34:27');
 
 -- --------------------------------------------------------
 
@@ -60,7 +61,7 @@ CREATE TABLE IF NOT EXISTS `employees` (
   `department_id` int(10) unsigned NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 --
 -- Dumping data for table `employees`
@@ -72,7 +73,8 @@ INSERT INTO `employees` (`id`, `employee_id`, `name`, `status`, `department_id`,
 (6, 150102, 'Michael Tarongoy', '1', 1, '2015-08-07 22:25:34', '2015-08-07 22:25:34'),
 (7, 140502, 'Nepthaly Talavera', '1', 2, '2015-08-08 06:00:48', '2015-08-08 06:00:48'),
 (8, 150501, 'Javin Jet Tevar', '1', 4, '2015-08-08 06:16:16', '2015-08-08 06:16:16'),
-(9, 1203020, 'Carlo Lozano', '1', 5, '2015-08-08 06:17:33', '2015-08-08 06:17:33');
+(9, 1203020, 'Carlo Lozano', '1', 5, '2015-08-08 06:17:33', '2015-08-08 06:17:33'),
+(10, 1108042, 'John C. Manlangit', '1', 1, '2015-08-10 00:42:22', '2015-08-10 00:42:22');
 
 -- --------------------------------------------------------
 
@@ -91,10 +93,10 @@ CREATE TABLE IF NOT EXISTS `employee_dtr` (
   `third_out` datetime DEFAULT NULL,
   `third_in` datetime DEFAULT NULL,
   `end_of_duty` datetime DEFAULT NULL,
-  `undertime` time NOT NULL,
-  `late` time NOT NULL,
-  `overbreak` time NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=460 DEFAULT CHARSET=latin1;
+  `undertime` time DEFAULT NULL,
+  `late` time DEFAULT NULL,
+  `overbreak` time DEFAULT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=822 ;
 
 -- --------------------------------------------------------
 
@@ -110,16 +112,16 @@ CREATE TABLE IF NOT EXISTS `employee_shifts` (
   `date_to` date DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `employee_shifts`
 --
 
 INSERT INTO `employee_shifts` (`id`, `employee_id`, `shift_id`, `date_from`, `date_to`, `created_at`, `updated_at`) VALUES
-(1, 3, 1, '2015-08-10', '2015-08-21', '2015-08-08 02:08:59', '0000-00-00 00:00:00'),
-(2, 4, 1, '2015-08-02', '2015-08-07', '2015-08-06 12:14:12', '0000-00-00 00:00:00'),
-(3, 3, 2, '2015-08-10', '2015-08-21', '2015-08-08 02:47:08', '0000-00-00 00:00:00');
+(1, 3, 1, '2015-07-01', '2015-07-16', '2015-08-10 01:12:36', '0000-00-00 00:00:00'),
+(2, 4, 1, '2015-07-01', '2015-07-16', '2015-08-10 01:12:54', '0000-00-00 00:00:00'),
+(3, 3, 2, '2015-07-01', '2015-07-16', '2015-08-10 01:13:04', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -130,24 +132,24 @@ INSERT INTO `employee_shifts` (`id`, `employee_id`, `shift_id`, `date_from`, `da
 CREATE TABLE IF NOT EXISTS `employee_shift_days` (
 `id` int(10) unsigned NOT NULL,
   `employee_shift_id` int(10) unsigned NOT NULL,
-  `day` enum('mon','tues','wed','thurs','fri','sat','sun') NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+  `day` enum('mon','tue','wed','thu','fri','sat','sun') NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
 
 --
 -- Dumping data for table `employee_shift_days`
 --
 
 INSERT INTO `employee_shift_days` (`id`, `employee_shift_id`, `day`) VALUES
-(8, 1, 'mon'),
-(9, 1, 'wed'),
-(10, 1, 'fri'),
-(11, 3, 'tues'),
-(12, 3, 'thurs'),
+(8, 3, 'mon'),
+(9, 3, 'wed'),
+(10, 3, 'fri'),
 (13, 2, 'mon'),
-(14, 2, 'tues'),
+(14, 2, 'tue'),
 (15, 2, 'wed'),
-(16, 2, 'thurs'),
-(17, 2, 'fri');
+(16, 2, 'thu'),
+(17, 2, 'fri'),
+(18, 3, 'tue'),
+(19, 3, 'thu');
 
 -- --------------------------------------------------------
 
@@ -195,15 +197,15 @@ CREATE TABLE IF NOT EXISTS `shifts` (
   `break` time NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `shifts`
 --
 
 INSERT INTO `shifts` (`id`, `description`, `shift_from`, `shift_to`, `working_hours`, `break`, `created_at`, `updated_at`) VALUES
-(1, 'Regular Shift (8:30 AM to 5:30 PM)', '08:30:00', '05:30:00', '08:00:00', '01:00:00', '2015-08-07 23:19:08', '0000-00-00 00:00:00'),
-(2, 'Afternoon Shift (2:00 PM to 10:00 PM)', '14:00:00', '22:00:00', '08:00:00', '01:00:00', '2015-08-08 02:48:02', '0000-00-00 00:00:00');
+(1, '8 Hour Shift (8:00 AM to 5:00 PM)', '07:00:00', '17:00:00', '08:00:00', '00:30:00', '2015-08-11 07:50:46', '0000-00-00 00:00:00'),
+(2, '8 Hour shift 2 (9:00 PM to 18:00 PM)', '09:00:00', '18:00:00', '09:00:00', '01:30:00', '2015-08-10 02:17:26', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -221,14 +223,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `username`, `password`, `user_type`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Rosemale-John II C. Villacorta', 'rosemalejohn@gmail.com', 'rosemalejohn', '$2y$10$A2/p.e7KpHfQvB3mmFchpOJQDmH/lAQiwtxokUHsiQ0UtjEXbCFJi', 'admin', 'XrDBxPrIkcHCctlczG93POeQ6LuGZX7iysg5n9nm39e8Nobr4SgT7Akq5lQS', '2015-08-04 06:08:12', '2015-08-08 16:00:03');
+(1, 'Rosemale-John II C. Villacorta', 'rosemalejohn@gmail.com', 'rosemalejohn', '$2y$10$A2/p.e7KpHfQvB3mmFchpOJQDmH/lAQiwtxokUHsiQ0UtjEXbCFJi', 'admin', 'ADltKX54Abshy1s6yTYO5NT8sbi8hSlTsGj8eXUP09fw8NiPkWXMfTnQqLa5', '2015-08-04 06:08:12', '2015-08-10 19:49:34'),
+(2, 'Michael Pena', 'michaelpena@gmail.com', '', '$2y$10$Gx30bO7P99/HMJ2cGrdos.UrsgufqAMM64OK.K5WgggX5k9hyezh.', 'admin', 'WzijTrYA47rdHsKBX2TKtCxfR40yaeTZeeIG1E9oGjHyVL5AN1YdLULdHaUp', '2015-08-10 00:43:54', '2015-08-10 00:45:40');
 
 --
 -- Indexes for dumped tables
@@ -290,17 +293,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `departments`
 --
 ALTER TABLE `departments`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `employees`
 --
 ALTER TABLE `employees`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `employee_dtr`
 --
 ALTER TABLE `employee_dtr`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=460;
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=822;
 --
 -- AUTO_INCREMENT for table `employee_shifts`
 --
@@ -310,7 +313,7 @@ MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 -- AUTO_INCREMENT for table `employee_shift_days`
 --
 ALTER TABLE `employee_shift_days`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=18;
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT for table `shifts`
 --
@@ -320,7 +323,7 @@ MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- Constraints for dumped tables
 --
